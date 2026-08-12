47bc2d6b5cfc44a8e24a2a0cec1edab15af8c7f4
lous9921 <lous526@yahoo.com>
2026-08-12 00:56:48 -0400

    Upgrade SkillSprint to production foundation

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"SkillSprint",description:"Micro-learning for in-demand skills"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
